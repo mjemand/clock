ddd893806e8592faa9f7105584b959521f52f9a7
mjemand <mechle.tamas@gmail.com>
2020-12-06 20:36:48 +0100

    ready

---
+++ main.js
@@ -0,0 +1,18 @@
+const getCurrentTime = () => {
+    const currentDate = new Date();
+    const hours = padNumbers(currentDate.getHours());
+    const minutes = padNumbers(currentDate.getMinutes());
+    const seconds = padNumbers(currentDate.getSeconds());
+    return `${[hours, minutes, seconds].join(':')}`
+};
+
+// Tíznél kisebb számok kiegészítése nullával:
+const padNumbers = (num) => {
+    return num < 10 ? `0${num}` : `${num}`;
+};
+
+setInterval(() => {
+    const time = getCurrentTime();
+    const clockFace = document.querySelector('.clock-face');
+    clockFace.textContent = time;
+}, 1000);
